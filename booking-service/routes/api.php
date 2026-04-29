be6b0b83ad83d7a3b1c52248c0f5bb95f357f3fb
JimJam2 <jiyudanjikiake03@gmail.com>
2026-04-29 11:34:47 +0700

    API fix

--- booking-service/routes/api.php
+++ booking-service/routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BookingController;
 
-Route::get('/bookings', [BookingController::class, 'index']);
-Route::post('/bookings', [BookingController::class, 'store']);
+Route::get('/bookings', function () {
+    return response()->json([
+        'message' => 'API booking jalan'
+    ]);
+});
+// Route::get('/bookings', [BookingController::class, 'index']);
+// Route::post('/bookings', [BookingController::class, 'store']);
